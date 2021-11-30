05d0e13f7e189a6bd80626dfa04a011f073937ec
MikhailEA <michev.work@gmail.com>
2021-11-30 18:40:08 +0300

    добавление экрана и активити

--- app/src/main/java/com/android/compass/MainActivity.java
+++ app/src/main/java/com/android/compass/MainActivity.java
@@ -2,13 +2,41 @@ package com.android.compass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private ImageView ivDinamic;
+    private TextView tvDegree;
+    private float curret_degree = 0f;
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        ivDinamic = findViewById(R.id.ivDinamic);
+        tvDegree =findViewById(R.id.tvDegree);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
